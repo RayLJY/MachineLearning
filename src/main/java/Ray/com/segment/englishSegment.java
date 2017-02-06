c1c692be72d976884c2c2804de076f211a9afb5a
Ray <lijianyu@breadtrip.com>
2017-02-06 19:25:54 +0800

    分词工具

---
+++ src/main/java/Ray/com/segment/englishSegment.java
@@ -0,0 +1,13 @@
+package Ray.com.segment;
+
+import java.io.Serializable;
+
+/**
+ * Created by ray on 17/2/6.
+ *
+ * This class be used to split English sentences to words
+ *
+ */
+public class englishSegment implements Serializable {
+
+}
