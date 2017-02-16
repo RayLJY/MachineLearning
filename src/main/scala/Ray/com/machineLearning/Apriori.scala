3fda8fcd671f69428133ed355d8ed6dbf09a5193
Ray <lijianyu@breadtrip.com>
2017-02-16 19:27:05 +0800

    FPGrowth

---
+++ src/main/scala/Ray/com/machineLearning/Apriori.scala
@@ -0,0 +1,8 @@
+package Ray.com.machineLearning
+
+/**
+  * Created by ray on 17/2/16.
+  */
+object Apriori {
+
+}
---
+++ src/main/scala/Ray/com/machineLearning/FPGrowth.scala
@@ -0,0 +1,48 @@
+package Ray.com.machineLearning
+
+import org.apache.spark.mllib.fpm.FPGrowth
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by ray on 17/2/16.
+  *
+  * The FP-growth algorithm is used to mine frequent pattern of movies in this object.
+  *
+  * we assume all movies that one people rating as a transaction,
+  * finally, all data will form a data set of transactions.
+  *
+  */
+object FPGrowth {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Test FPGrowth").setMaster("local")
+    val sc = new SparkContext(conf)
+    val path = "data/ml-100k/u.data"
+    val ratingRdd = sc.textFile(path).map(_.split("\t")).map { l => rating(l(0), l(1), l(2), l(3)) }
+
+    val rdd = ratingRdd.map{ r => (r.userId, r.itemId)}.groupByKey.map(_._2.toArray)
+
+    val minSupport = 0.2
+    val minConfidence = 0.8
+
+    val fpg = new FPGrowth()
+      .setMinSupport(minSupport)
+      .setNumPartitions(10)
+    val model = fpg.run(rdd)
+
+    //print frequent item and count
+    model.freqItemsets.collect().foreach { itemSet =>
+      println(itemSet.items.mkString("[", ",", "]") + ", " + itemSet.freq)
+    }
+
+    model.generateAssociationRules(minConfidence).collect().foreach { rule =>
+      println(
+        rule.antecedent.mkString("[", ",", "]")
+          + " => " + rule.consequent.mkString("[", ",", "]")
+          + ", " + rule.confidence)
+    }
+  }
+
+  case class rating(userId: String, itemId: String, rating: String, timestamp: String)
+
+}
